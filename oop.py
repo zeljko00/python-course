712db721bacc84ac786d575a33cff82a314845f8
Zeljko Tripic <ztripic@domain.local>
2023-10-12 10:19:23 +0200

    class names notation fixed

--- oop.py
+++ oop.py
@@ -1,25 +1,25 @@
-class Test_class:
+class TestClass:
     class_prop=0
     def __init__(self,id, value):
         # leading underscore in attribute name is convention for attributes intended to be private
         self._id=id
         self.value=value
-        Test_class.class_prop+=1
+        TestClass.class_prop+=1
     def method(self):
-        print("Test_class method call!")
+        print("TestClass method call!")
 
-        print("Accessing class' attribute from method ->",Test_class.class_prop)
+        print("Accessing class' attribute from method ->",TestClass.class_prop)
         # accessing class' attribute via object reference is possible, but not recommended
         print("Accessing class' attribute from method via object reference ->",self.class_prop)
 
         print("Accessing static method from method ->")
-        Test_class.helper_static_method()
+        TestClass.helper_static_method()
         # accessing static methods via object reference is possible, but not recommended
         print("Accessing static method from method via object reference ->")
         self.helper_static_method()
 
         print("Accessing class method from method ->")
-        Test_class.helper_class_method()
+        TestClass.helper_class_method()
         # accessing class methods via object reference is possible, but not recommended
         print("Accessing class method from method via object reference ->")
         self.helper_class_method()
@@ -27,33 +27,33 @@ class Test_class:
         print("Accessing object's attribute from method ->",self.value)
         # accessing object attribute via class name is not possible,
         # because object attribute is connected to specific class instance (object)
-        # print("Accessing object's attribute from method via class name ->",Test_class.value)
+        # print("Accessing object's attribute from method via class name ->",TestClass.value)
 
         print("Accessing object's method from another method ->")
         self.helper_method()
         # accessing method via class name is not possible
         # print("Accessing object's method from another method via class name->")
-        # Test_class.helper_method()
+        # TestClass.helper_method()
 
         print("Accessing object's property from method ->", self.helper_prop)
         # accessing property via class name return reference to the property function
-        print("Accessing object's property from method via class name->", Test_class.helper_prop)
+        print("Accessing object's property from method via class name->", TestClass.helper_prop)
 
 
     def helper_method(self):
-        print("Test_class helper_method call!")
+        print("TestClass helper_method call!")
 
     @staticmethod
     def static_method():
-        print("Test_class static method call!")
+        print("TestClass static method call!")
 
-        print("Accessing class' attribute from static method ->", Test_class.class_prop)
+        print("Accessing class' attribute from static method ->", TestClass.class_prop)
 
         print("Accessing static method from another static method ->")
-        Test_class.helper_static_method()
+        TestClass.helper_static_method()
 
         print("Accessing class method from static method ->")
-        Test_class.helper_class_method()
+        TestClass.helper_class_method()
 
         # self reference is not accessible from static method
         # print("Accessing class' attribute from static method via object reference ->", self.class_prop)
@@ -67,28 +67,28 @@ class Test_class:
         # print("Accessing object's property from static method ->", self.helper_prop)
 
         # accessing object's attributes, properties and methods via class name is not possible from anywhere
-        # print("Accessing object's attribute from static method ->", Test_class.value)
+        # print("Accessing object's attribute from static method ->", TestClass.value)
         # print("Accessing object' method from static method")
-        # Test_class.helper_method()
+        # TestClass.helper_method()
 
         # accessing property via class name return reference to the property function
-        print("Accessing object's property from static method via class name->", Test_class.helper_prop)
+        print("Accessing object's property from static method via class name->", TestClass.helper_prop)
 
     @staticmethod
     def helper_static_method():
-        print("Test_class helperstatic method call!")
+        print("TestClass helperstatic method call!")
 
     @classmethod
     def class_method(cls):
-        print("Test_class class method call!")
+        print("TestClass class method call!")
 
-        print("Accessing class' attribute from class method ->", Test_class.class_prop)
+        print("Accessing class' attribute from class method ->", TestClass.class_prop)
 
         print("Accessing static method from class static method ->")
-        Test_class.helper_static_method()
+        TestClass.helper_static_method()
 
         print("Accessing class method from another class method ->")
-        Test_class.helper_class_method()
+        TestClass.helper_class_method()
 
         # self reference is not accessible from class method
         # print("Accessing class' attribute from class method via object reference ->", self.class_prop)
@@ -102,33 +102,33 @@ class Test_class:
         # print("Accessing object's property from class method ->", self.helper_prop)
 
         # accessing object's attributes, properties and methods via class name is not possible from anywhere
-        # print("Accessing object's attribute from class method ->", Test_class.value)
+        # print("Accessing object's attribute from class method ->", TestClass.value)
         # print("Accessing object' method from class method")
-        # Test_class.helper_method()
+        # TestClass.helper_method()
 
         # accessing property via class name return reference to the property function
-        print("Accessing object's property from class method via class name->", Test_class.helper_prop)
+        print("Accessing object's property from class method via class name->", TestClass.helper_prop)
 
     @classmethod
     def helper_class_method(cls):
-        print("Test_class helper class method call!")
+        print("TestClass helper class method call!")
 
     @property
     def prop(self):
-        print("Test_class property call!")
+        print("TestClass property call!")
 
-        print("Accessing class' attribute from property ->", Test_class.class_prop)
+        print("Accessing class' attribute from property ->", TestClass.class_prop)
         # accessing class' attribute via object reference is possible, but not recommended
         print("Accessing class' attribute from property via object reference ->", self.class_prop)
 
         print("Accessing static method from property ->")
-        Test_class.helper_static_method()
+        TestClass.helper_static_method()
         # accessing static methods via object reference is possible, but not recommended
         print("Accessing static method from property via object reference ->")
         self.helper_static_method()
 
         print("Accessing class method from property ->")
-        Test_class.helper_class_method()
+        TestClass.helper_class_method()
         # accessing class methods via object reference is possible, but not recommended
         print("Accessing class method from property via object reference ->")
         self.helper_class_method()
@@ -136,34 +136,34 @@ class Test_class:
         print("Accessing object's attribute from property ->", self.value)
         # accessing object attribute via class name is not possible,
         # because object attribute is connected to specific class instance (object)
-        # print("Accessing object's attribute from property via class name ->", Test_class.value)
+        # print("Accessing object's attribute from property via class name ->", TestClass.value)
 
         print("Accessing object's method from property ->")
         self.helper_method()
         # accessing method via class name is not possible
         # print("Accessing object's method from property via class name->")
-        # Test_class.helper_method()
+        # TestClass.helper_method()
 
 
         # accessing property via class name return reference to the property function
-        print("Accessing object's property from property via class name->", Test_class.helper_prop)
+        print("Accessing object's property from property via class name->", TestClass.helper_prop)
 
         print("Accessing object's property from another property ->", self.helper_prop)
         return self.helper_prop  # property should return some value
 
     @property
     def helper_prop(self):
-        print("Test_class helper property call!")
+        print("TestClass helper property call!")
         return "#"+str(self.value)
 
     @property
     def id(self):
-        print("Test_class id property call!")
+        print("TestClass id property call!")
         return self._id
     # setter must have same name as corresponding property
     @id.setter
     def id(self,value):
-        print("Test_class id setter call!")
+        print("TestClass id setter call!")
         self._id=value
 
 # specifying str(object) behavior, equivalent to toString() method in Java
@@ -191,22 +191,22 @@ class A(Abstract_class):
 
 
 
-test_object=Test_class(13,"VALUE")
-Test_class.static_method()
+test_object=TestClass(13,"VALUE")
+TestClass.static_method()
 # accessing static method via object reference is not recommended
 test_object.static_method()
-Test_class.class_method()
+TestClass.class_method()
 # accessing class method via object reference is not recommended
 test_object.class_method()
-print(Test_class.class_prop)
+print(TestClass.class_prop)
 # accessing class attribute via object reference is not recommended
 print(test_object.class_prop)
 print(test_object.value)
 # accessing object attribute via class name is not possible
-# print(Test_class.value)
+# print(TestClass.value)
 test_object.method()
 # accessing method via class name is not possible
-# Test_class.method()
+# TestClassTestClass.method()
 print(test_object.prop)
 print(test_object.id)
 # using setter method
